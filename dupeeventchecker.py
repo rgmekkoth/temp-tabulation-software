2d8e8fb1d86c4cec844031bb0b138d48cd3faae2
James Kooiman <94080447+JKooiman@users.noreply.github.com>
2022-07-01 16:40:12 -0700

    Create dupeeventchecker.py
    Checks for duplicate/no events in every row

---
+++ dupeeventchecker.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vSAH0q4IKYBmA46IT2IbrjpuxwwmOlKUcAZSaaFWa_iiv0Iv6sER08JDWAy0wJ8mjdmobE9OORIyjft/pub?output=xlsx")
+
+eventindex1 = 4 #First index in the spreadsheet where event abbreviations begin
+eventindex2 = 12#Last index in the spreadsheet where event abbreviations begin
+
+def checkdupeevents(df):
+    good = True
+    for i in range(len(df.index)):
+        n = 0
+        for j in range(4,13):
+            if isinstance(df.iat[i,j],str):
+                n = n + 1
+        if n == 0:
+            print("No events detected on row " + str(i + 2))
+            good = False
+        elif n > 1:
+            print("Too many events detected on row " + str(i + 2))
+            good = False
+    if good:
+        print("No problems were detected!")
+
+
+checkdupeevents(df)
