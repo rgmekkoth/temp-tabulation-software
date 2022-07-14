44bdfde107f196d0676e76458aa7729ccff8a9d0
James Kooiman <94080447+JKooiman@users.noreply.github.com>
2022-07-13 19:32:47 -0700

    Update exam.py
    Gets the percentage directly

--- exam.py
+++ exam.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-examdf = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5IDq9qak5JmrcXra2_UKYR3HRegeWccDSnLEtgsc4cvjXarEtZ3uVrWC4fPXf_A/pub?output=xlsx")
+df = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5IDq9qak5JmrcXra2_UKYR3HRegeWccDSnLEtgsc4cvjXarEtZ3uVrWC4fPXf_A/pub?output=xlsx")
 
-#returns exam score for a student given a student id. 
+#returns exam score for a student given a student id.
 def getexam(studentid):
-    for i in range(len(examdf.index)):
-        if (examdf.iat[i,1]) == studentid:
-            return round(100*((examdf.iat[i,5])/45),2)
+    for i in range(len(df.index)):
+        if (df.iat[i,1]) == studentid:
+            return (100*df.iat[i,4])
     return 0
 
 print(getexam(16885))
